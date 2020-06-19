829506f156e89d332fcb4084d2f8e8df3bd233e2
johnmyronmillar <johnmyronmillar@gmail.com>
2020-06-19 13:22:26 -0500

    start hw

---
+++ Code_files/QuickDBD-export.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "card_holder" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(60)   NOT NULL,
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "credit_card" (
+    "card" BIGINT   NOT NULL,
+    "id_card_holder" INT   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+CREATE TABLE "transaction" (
+    "id" INT   NOT NULL,
+    "date" TIMESTAMP   NOT NULL,
+    "amount" FLOAT   NOT NULL,
+    "card" BIGINT   NOT NULL,
+    "id_merchant" INT   NOT NULL,
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(60)   NOT NULL,
+    "id_merchant_category" INT   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant_category" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_id_card_holder" FOREIGN KEY("id_card_holder")
+REFERENCES "card_holder" ("id");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
+
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merchant_category" FOREIGN KEY("id_merchant_category")
+REFERENCES "merchant_category" ("id");
+
---
+++ Code_files/Select_From.sql
@@ -0,0 +1,9 @@
+SELECT * FROM card_holder;
+
+SELECT * FROM credit_card;
+
+SELECT * FROM merchant;
+
+SELECT * FROM merchant_category;
+
+SELECT * FROM transaction;
